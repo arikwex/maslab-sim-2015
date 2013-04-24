2f322d6a948c3003c060fd9757783263d8d52d29
Alex Lesman <lesman@mit.edu>
2013-04-24 18:25:34 -0400

    Stubbing out mapping

---
+++ src/map/Map.java
@@ -0,0 +1,30 @@
+package map;
+
+public class Map {
+    ArrayList<Obstacle> obstacles;
+    ArrayList<Blocks> blocks;
+
+    Robot bot;
+
+    public Map(bot, margin) {
+
+    }
+
+    public boolean addBlock() {
+
+    }
+
+    public void moveRobot(double x, double y, double theta) {
+
+    }
+
+    public void checkSegment(Segment seg) {
+
+    }
+
+    public void nearestIntersectingSegment(Segment seg) {
+
+    }
+
+
+}
