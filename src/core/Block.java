b985ca18218162ab4239786e66e7bfac9d9c9c76
Jesika Haria <jesikaharia@gmail.com>
2013-04-25 00:44:46 -0400

    Block.java updated

--- src/core/Block.java
+++ src/core/Block.java
@@ -4,26 +4,23 @@ import core.Config.Color;
 
 public class Block {
 
-    
     public double x;
     public double y;
     
     public double relX;
-    public double relY;
-    
-    public int sizeP;
-
+    public double relY; 
+    public int sizeP; 
     public Color color;
-    public long time;
     
-    public Block(double x, double y, Color color) {
-        this.x = x;
-        this.y = y;
-        this.color= color;
+    public Block(double relX, double relY, int sizeP, Color color){
+    	this.relX = relX; 
+    	this.relY = relY; 
+    	this.sizeP = sizeP; 
+    	this.color = color; 
     }
-    
-    
 
+    // setPosition Transforms to coordinate positions of the world. 
+    // Taking into account the angle of the robot to the world. 
     public void setPosition(double botX, double botY, double botTheta) {
         double r =Math.sqrt( Math.pow(this.relX,2) + Math.pow(this.relY,2));
 
@@ -31,19 +28,6 @@ public class Block {
 
         x = botX + r * Math.cos(phi + botTheta);
         y = botY + r * Math.sin(phi + botTheta);
-    }
-
-    
-
-    public double distanceToBlock(Block otherBlock){
-        double deltaX = this.x - otherBlock.x;
-        double deltaY = this.y - otherBlock.y;
-        return Math.sqrt(Math.pow(deltaX,2) + Math.pow(deltaY,2));
-    }
-
-    public double distanceTo(double x, double y){
-     
-        return Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
     }
 
 }
