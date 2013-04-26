cdc29b822438005ff7d67677ed747daef47d2e2b
Chewy Shaw <chinuas@mit.edu>
2013-04-26 15:26:13 -0400

    added tree andtreenode

---
+++ src/utils/Tree.java
@@ -0,0 +1,20 @@
+package utils;
+
+import java.util.LinkedList;
+
+public class Tree {
+	public LinkedList<TreeNode> nodes;
+	TreeNode root;
+
+	public Tree(TreeNode root) {
+		nodes = new LinkedList<TreeNode>();
+		this.root = root;
+		nodes.add(root);
+		root.tree = this;
+	}
+
+	public void addNode(TreeNode node) {
+		nodes.add(node);
+	}
+
+}
