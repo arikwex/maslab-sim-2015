919695ad3d7e012389c9af48dea07503fe1ed402
Chewy Shaw <chinuas@mit.edu>
2013-04-27 02:06:25 -0400

    CaptureImage now constantly takes a new picture and saves it to "capture.jpg"

--- src/core/CaptureImage.java
+++ src/core/CaptureImage.java
@@ -1,45 +1,34 @@
 package core;
 
 import com.googlecode.javacv.CanvasFrame;
-import com.googlecode.javacv.FrameGrabber;
 import com.googlecode.javacv.OpenCVFrameGrabber;
 import com.googlecode.javacv.cpp.opencv_core.IplImage;
+import static com.googlecode.javacv.cpp.opencv_highgui.*;
 
-public class CaptureImage {
-    
-    IplImage image;
-    static CanvasFrame canvas = new CanvasFrame("Web Cam");
-    
-    public CaptureImage() {
-        canvas.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
-    }
-    
-    private static void captureFrame(FrameGrabber grabber) {
+public class CaptureImage implements Runnable {
+
+    static IplImage img;
+	static IplImage image;
+    private static void captureFrame() {
+        // 0-default camera, 1 - next...so on
+        final OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
         try {
-            IplImage img = grabber.grab();
+            grabber.start();
+            img = grabber.grab();
             if (img != null) {
-                canvas.showImage(img);
+                cvSaveImage("capture.jpg", img);
+                
+                image = cvLoadImage("capture.jpg");
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println(System.getProperty("java.library.path"));
-
-        final OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
-        //final VideoInputFrameGrabber grabber = new VideoInputFrameGrabber(0);
-        
-        try {
-            grabber.start();
-        } catch (com.googlecode.javacv.FrameGrabber.Exception e) {
-            e.printStackTrace();
-        }
-
-
-        while(true){
-            captureFrame(grabber); 
-            Thread.sleep(0);
-        }
-    }     
+    
+	@Override
+	public void run() {
+		while (true){
+    		captureFrame();
+    	}
+	}
 }
