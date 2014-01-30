9e812c0cbcb7851be972e3c44b783adc5e88ff1c
John Bowler <jwbowler@gmail.com>
2014-01-30 03:21:07 -0500

    added demo java project

---
+++ demo/src/devices/sensors/DigitalInput.java
@@ -0,0 +1,44 @@
+package devices.sensors;
+
+import java.nio.ByteBuffer;
+
+import devices.Sensor;
+
+public class DigitalInput extends Sensor {
+	
+	byte pin;
+	boolean val;
+	
+	/*
+	 * Takes one digital pin
+	 */
+	public DigitalInput(int pin) {
+		this.pin = (byte) pin;
+	}
+
+	@Override
+	public byte getDeviceCode() {
+		return 'D';
+	}
+
+	@Override
+	public byte[] getInitializationBytes() {
+		return new byte[] {pin};
+	}
+
+	@Override
+	public void consumeMessageFromMaple(ByteBuffer buff) {
+		val = (buff.get() != 0);
+		System.out.println("Consuming value from Maple: " + val);
+	}
+
+	@Override
+	public int expectedNumBytesFromMaple() {
+		return 1;
+	}
+	
+	public boolean getValue() {
+		return val;
+	}
+
+}
