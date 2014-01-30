efbf76374497caa81cf6b2b5828ba5d8dcfbc34f
arikwex <arikwex@mit.edu>
2014-01-30 16:09:12 -0500

    Vision code started

--- src/vision/VisionInterface.java
+++ src/vision/VisionInterface.java
@@ -3,5 +3,10 @@ package vision;
 import java.util.ArrayList;
 
 public interface VisionInterface {
-    public ArrayList<Ball> getBalls();
+	
+	public void snapshot();				// Applies a snapshot and analyzes the image
+	
+    public ArrayList<Ball> getBalls();	// Returns a list of balls (r,theta,color)
+    
+    public ArrayList<Wall>	getWalls();	// Returns a list of walls (endpoints, type, normal) 
 }
